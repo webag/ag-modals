362defcf49ea10878c4bb641a231ae09002128d1
Андрей <andreiduffy@gmail.com>
2015-03-17 13:54:18 +0300

    Rebuild js. Adding multiple windows.

--- agmodal/agmodal.js
+++ agmodal/agmodal.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	$('.agmodal').bind('ag:open');
+
 	$('*[data-ag]').on('click', function(e) {
 		e.preventDefault();
 		var modalLocation = $(this).attr('data-ag');
@@ -6,25 +8,27 @@ $(document).ready(function(){
 		modal.trigger('ag:open');
 	});
 
-	$('.agmodal').bind('ag:open',function(){
-		$('<div class="agmodal-bg"></div>').insertAfter(this);
+	$('.agmodal').on('ag:open', function() {
+		var id_modal = $(this).attr('id');
+		var id_bg = "bg_for_"+id_modal;
+		$('<div class="agmodal-bg" id="'+id_bg+'"></div>').insertAfter(this);
 		$(this).addClass('ag-visible');
-		$('.agmodal-bg').fadeIn(400);
-
+		$('#'+id_bg).fadeIn(400);
 		var scrolled = $(window).scrollTop()+50;
-		$('.agmodal').css('top', scrolled);
+		$(this).css('top', scrolled);
+
+		$('.close-agmodal').click(function(){$(this).trigger('ag:close');});
+		$('.agmodal-bg').click(function(){$('#'+id_modal).trigger('ag:close');});
 
 		$(this).bind('ag:close', function () {
 			$(this).removeClass('ag-visible');
-			$('.agmodal-bg').fadeOut(400);
-			setTimeout(function() {$('.agmodal-bg').remove();},400);
+			$('#'+id_bg).fadeOut(400);
+			setTimeout(function() {$('#'+id_bg).remove();},400);
 
 			if($('.agmodal video').length){
 				$('.agmodal video').get(0).pause();
 			}
 		})
-
-		$('.close-agmodal,.agmodal-bg').click(function(){$(this).trigger('ag:close');});
-	})
+	});
 
 })
